c152fed86a4d9079d132272843dab08a108e7354
Evan Duncan <evan.e.duncan@gmail.com>
2018-08-08 11:01:06 -0500

    Extra extra credit

--- lab-02-first-component/src/App.test.js
+++ lab-02-first-component/src/App.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import Hello from './hello/Hello';
 
 describe('App Component', () => {
   it('renders without exploding', () => {
     expect(shallow(<App />)).toHaveLength(1);
+  });
+
+  it('should render a hello component', () => {
+    expect(shallow(<App />).contains(<Hello friend="Woooohoooo!!!" />)).toBeTruthy();
   });
 });
